9325f04f9f91c694d9cdf926d451b77109fdaca9
nitika2205 <knitika567@gmail.com>
2026-03-31 18:16:01 +0530

    Basic types in C.

---
+++ basic_types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int max_recursive_calls = 100;
+    char mode = 'S';
+    float throttle_speed = 0.9;
+    printf("Max recursive calls: %d\n", max_recursive_calls);
+  printf("IO mode: %c\n", mode);
+  printf("Throttle speed: %f\n", throttle_speed);
+  return 0;
+}
+
+
