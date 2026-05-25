6a4a6165c0f43a75d46c280e6c7acd8f71c4bcd7
nitika2205 <knitika567@gmail.com>
2026-05-25 15:49:42 +0530

    use of free function in memory allocation.

---
+++ free.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *allocate_scalar_list(int size, int multiplier) {
+  int *lst = malloc(size * sizeof(int));
+  if (lst == NULL) {
+    return NULL;
+  }
+  for (int i = 0; i < size; i++) {
+    lst[i] = i * multiplier;
+  }
+  return lst;
+}
+
+int main() {
+  const int num_lists = 500;
+  for (int i = 0; i < num_lists; i++) {
+    int *lst = allocate_scalar_list(50000, 2);
+    if (lst == NULL) {
+      printf("Failed to allocate list\n");
+      return 1;
+    } else {
+      printf("Allocated list %d\n", i);
+    }
+    free(lst);
+  }
+  return 0;
+}
